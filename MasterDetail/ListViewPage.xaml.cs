528144573d235fd1b74c8bcc91ad82e11e592d71
Mario Galvan <mario@orionsdev.com>
2016-05-25 14:25:40 -0500

    Removed Entitlements.plist from .csproj

---
+++ MasterDetail/ListViewPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+	public partial class ListViewPage : ContentPage
+	{
+		public ListViewPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
